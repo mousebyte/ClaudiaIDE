30297298fce49aff80992a095c99660e2fe0ff7a
mousebyte <ateague063@gmail.com>
2020-09-07 22:21:29 -0400

    Cleanup and organize code

--- ClaudiaIDE/ClaudiaIDEFactory.cs
+++ ClaudiaIDE/ClaudiaIDEFactory.cs
@@ -1,18 +1,17 @@
+using System;
 using System.ComponentModel.Composition;
-using ClaudiaIDE.Settings;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Utilities;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.Text.Classification;
 
 namespace ClaudiaIDE
 {
     #region Adornment Factory
 
     /// <summary>
-    /// Establishes an <see cref="IAdornmentLayer"/> to place the adornment on and exports the <see cref="IWpfTextViewCreationListener"/>
-    /// that instantiates the adornment on the event of a <see cref="IWpfTextView"/>'s creation
+    ///     Establishes an <see cref="IAdornmentLayer" /> to place the adornment on and exports the
+    ///     <see cref="IWpfTextViewCreationListener" />
+    ///     that instantiates the adornment on the event of a <see cref="IWpfTextView" />'s creation
     /// </summary>
     [Export(typeof(IWpfTextViewCreationListener))]
     [ContentType("text")]
@@ -20,21 +19,21 @@ namespace ClaudiaIDE
     [TextViewRole(PredefinedTextViewRoles.Document)]
     internal sealed class ClaudiaIDEAdornmentFactory : IWpfTextViewCreationListener
     {
-        [Import(typeof(SVsServiceProvider))] internal System.IServiceProvider ServiceProvider { get; set; }
-
         /// <summary>
-        /// Defines the adornment layer for the scarlet adornment. This layer is ordered 
-        /// after the selection layer in the Z-order
+        ///     Defines the adornment layer for the scarlet adornment. This layer is ordered
+        ///     after the selection layer in the Z-order
         /// </summary>
         [Export(typeof(AdornmentLayerDefinition))]
         [Name("ClaudiaIDE")]
         [Order(Before = PredefinedAdornmentLayers.DifferenceChanges)]
         public AdornmentLayerDefinition EditorAdornmentLayer { get; set; }
 
+        [Import(typeof(SVsServiceProvider))] internal IServiceProvider ServiceProvider { get; set; }
+
         /// <summary>
-        /// Instantiates a ClaudiaIDE manager when a textView is created.
+        ///     Instantiates a ClaudiaIDE manager when a textView is created.
         /// </summary>
-        /// <param name="textView">The <see cref="IWpfTextView"/> upon which the adornment should be placed</param>
+        /// <param name="textView">The <see cref="IWpfTextView" /> upon which the adornment should be placed</param>
         public void TextViewCreated(IWpfTextView textView)
         {
             new ClaudiaIDE(textView);
